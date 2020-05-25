3621b10df7e8e0dd1676f16da7c2e49a58ffd9fe
Duvan <duvan4448@gmail.com>
2020-05-25 17:34:57 -0500

    2-Migraciones-con-Code-First

---
+++ EmpleadosMVC/EmpleadosMVC/Migrations/202005252223421_Initial.cs
@@ -0,0 +1,29 @@
+namespace EmpleadosMVC.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Initial : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Empleadoes",
+                c => new
+                    {
+                        ID = c.Int(nullable: false, identity: true),
+                        Nombre = c.String(nullable: false, maxLength: 50),
+                        Antiguedad = c.Int(nullable: false),
+                        Edad = c.Int(nullable: false),
+                        Categoria = c.String(nullable: false),
+                    })
+                .PrimaryKey(t => t.ID);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Empleadoes");
+        }
+    }
+}
